591fff6a19657589fa2c7e3cc976e6ea364428e0
Cassio Medeiros <cassio.medeiros19@gmail.com>
2026-09-04 09:53:45 -0300

    Envio do algoritmo oito versão do aluno, objetivo: Ler dois números inteiros e imprimir o produto

---
+++ src/AlgoritmoOitoAl.java
@@ -0,0 +1,8 @@
+public class AlgoritmoOitoAl {
+    void main () {
+        int numero1 = Integer.parseInt(IO.readln("Digite o primeiro número: "));
+        int numero2 = Integer.parseInt(IO.readln("Digite o segundo número: "));
+
+        IO.println(numero1 + " e " + numero2);
+    }
+}
