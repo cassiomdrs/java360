e8b65171553c8d4854031b919ca5257d597a3e22
Cassio Medeiros <cassio.medeiros19@gmail.com>
2026-09-04 09:47:30 -0300

    Envio do algoritmo sete versão aluno: ler dois números inteiros e imprimar na tela a soma deles

---
+++ src/AlgoritmoSete.java
@@ -0,0 +1,11 @@
+public class AlgoritmoSete {
+    void main () {
+        IO.println("Vamos somar dois números inteiros!\n");
+
+        int primeiroNumero = Integer.parseInt(IO.readln("Digite o primeiro número inteiro: "));
+        int segundoNumero = Integer.parseInt(IO.readln("Digite o segundo número inteiro: "));
+        int soma = primeiroNumero + segundoNumero;
+
+        IO.println("Soma: " + primeiroNumero + " + " + segundoNumero + " = " + soma);
+    }
+}
