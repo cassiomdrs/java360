a01f5172fec227025ba50d0fbe0f1da14655f431
Cassio Medeiros <cassio.medeiros19@gmail.com>
2026-09-04 09:40:00 -0300

    Envio do algoritmo seis versão aluno atendendo a proposta de ler nome, endereço e telefone e imprimi-los na tela

---
+++ src/AlgoritmoSeisAl.java
@@ -0,0 +1,11 @@
+public class AlgoritmoSeisAl {
+    void main () {
+        String nome = IO.readln("Digite seu nome: ");
+        String endereco = IO.readln("Digite seu endereço: ");
+        String telefone = IO.readln("Digite seu número de telefone: ");
+
+        IO.println("\nNome: " + nome);
+        IO.println("Endereço: " + endereco);
+        IO.println("Telefone: " + telefone);
+    }
+}
