d8c05e28441ab891a784483eb53245b590363d60
Cassio Medeiros <cassio.medeiros19@gmail.com>
2026-09-09 09:27:41 -0300

    Algoritmo dezoito versão do aluno = Crie um algoritmo que mostre os números pares de 0 a 20

---
+++ src/AlgoritmoDezoitoAl.java
@@ -0,0 +1,12 @@
+public class AlgoritmoDezoitoAl {
+    public void main () {
+        int[] numeros = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+        for (int valor : numeros) {
+            float resto = valor % 2;
+            if (resto == 0) {
+                IO.println(valor);
+            }
+        }
+    }
+}
