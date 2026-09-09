a9f05ce3be30b36bba7984496b6c191139449f1b
Cassio Medeiros <cassio.medeiros19@gmail.com>
2026-09-09 09:35:42 -0300

    Algoritmo dezenove versão do aluno = Imprimir os nomes de uma lista

---
+++ src/AlgoritmoDezenoveAl.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class AlgoritmoDezenoveAl {
+    public void main () {
+        List<String> usuarios = List.of("Ana","Bruno","Carla","Diego");
+        usuarios.forEach(nomes -> IO.println(nomes));
+    }
+}
