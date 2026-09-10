65b5d0a3374d2a19ad3ad423946be743746fa5df
Cassio Medeiros <cassio.medeiros19@gmail.com>
2026-09-10 11:49:19 -0300

    Algoritmo 30 versão do aluno = Descobrir o fatoria de um número.

---
+++ src/AlgoritmoTrintaAl.java
@@ -0,0 +1,13 @@
+public class AlgoritmoTrintaAl {
+    public void main () {
+        int numeroFatorial = Integer.parseInt(IO.readln("Digite o número que deseja descobrir o fatorial: "));
+        int[] decremento = new int [numeroFatorial];
+        int calcular = 1;
+
+        for (int i = decremento.length - 1; i >= 0; i--) {
+            decremento[i] = i+1;
+            calcular = calcular * decremento[i];
+        }
+        IO.println("Resultado: " + calcular);
+    }
+}
