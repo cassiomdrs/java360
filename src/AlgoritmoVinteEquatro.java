95c05e397477e08346696d9823e73391a35c15a9
Cassio Medeiros <cassio.medeiros19@gmail.com>
2026-09-10 09:42:57 -0300

    Envio do algoritmo 23 e 24 versão do aluno mostrando o recebimento de valores em cinco variáveis e a utilização de vetores para facilitar a inserção de valores nas variáveis

---
+++ src/AlgoritmoVinteEquatro.java
@@ -0,0 +1,13 @@
+public class AlgoritmoVinteEquatro {
+    public void main () {
+        int[] numeros = new int[5];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = Integer.parseInt(IO.readln("Entre com o número da variável " + (i+1) + ": "));
+        }
+
+        for (int i = 0; i < numeros.length; i++) {
+            IO.println("Variável " + (i+1) + " = " + numeros[i]);
+        }
+    }
+}
---
+++ src/AlgoritmoVinteEtres.java
@@ -0,0 +1,17 @@
+public class AlgoritmoVinteEtres {
+    public void main () {
+        IO.println("*** PREENCHIMENTO DE VARIÁVEIS SIMPLES ***");
+
+        int numero1 = Integer.parseInt(IO.readln("Entre com o número da variável 1: "));
+        int numero2 = Integer.parseInt(IO.readln("Entre com o número da variável 2: "));
+        int numero3 = Integer.parseInt(IO.readln("Entre com o número da variável 3: "));
+        int numero4 = Integer.parseInt(IO.readln("Entre com o número da variável 4: "));
+        int numero5 = Integer.parseInt(IO.readln("Entre com o número da variável 5: "));
+
+        IO.println("Variável 1 = " + numero1);
+        IO.println("Variável 2 = " + numero2);
+        IO.println("Variável 3 = " + numero3);
+        IO.println("Variável 4 = " + numero4);
+        IO.println("Variável 5 = " + numero5);
+    }
+}
