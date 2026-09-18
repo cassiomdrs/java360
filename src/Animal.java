1a775cc217fa2c2781020fac611cf792d2ceafbe
Cassio Medeiros <cassio.medeiros19@gmail.com>
2026-09-18 11:37:51 -0300

    Apagando imports adicionados indevidamente

--- src/Animal.java
+++ src/Animal.java
@@ -1,8 +1,3 @@
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import java.io.File;
-
 public abstract class Animal {
     private String nome;
     private String tocarSom;
