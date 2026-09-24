a3802105b27c3e79efb75d0be7f6b46bfd2c54c7
Cassio Medeiros <cassio.medeiros19@gmail.com>
2026-09-24 16:41:51 -0300

    Retirando a variável contador pois ela não era necessária e reduzindo o código

--- src/Algoritmo47.java
+++ src/Algoritmo47.java
@@ -4,7 +4,6 @@ public class Algoritmo47 {
     public void main (){
         double[] numeros = new double[10]; // Criei um vetor com capacidade para armazenar 10 números
         double soma = 0; // Variável soma para receber os valores de cada número digitado
-        int contador = 0; // Variável para contar quantos espaços tem no vetor
 
         for(int i = 0; i < numeros.length; i++){
             //int numeroDigitado = Integer.parseInt(IO.readln("Posição " + (i+1) + " selecionada, digite o número: "));
@@ -12,10 +11,9 @@ public class Algoritmo47 {
             numeros[i] = Integer.parseInt(JOptionPane.showInputDialog(null, (i + 1) + "º Número = Digite um número:", "Inserir 10 números", JOptionPane.QUESTION_MESSAGE));
 
             soma += numeros[i];
-            contador++;
         }
         
-        double media = soma / contador;
+        double media = soma / numeros.length;
 
         //IO.println("A SOMA de todos os números inseridos é: " + soma);
         //IO.println("A MÉDIA dos números é: " + media);
