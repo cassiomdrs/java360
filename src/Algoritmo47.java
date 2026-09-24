722f784e12b1f63821d885cdc319dbc40d8bb57b
Cassio Medeiros <cassio.medeiros19@gmail.com>
2026-09-24 16:05:00 -0300

    Criar um algoritmo que recebe 10 números dentro de um vetor e mostrar a médias dos números

---
+++ src/Algoritmo47.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class Algoritmo47 {
+    public void main (){
+        double[] numeros = new double[10]; // Criei um vetor com capacidade para armazenar 10 números
+        double soma = 0; // Variável soma para receber os valores de cada número digitado
+        int contador = 0; // Variável para contar quantos espaços tem no vetor
+
+        for(int i = 0; i < numeros.length; i++){
+            //int numeroDigitado = Integer.parseInt(IO.readln("Posição " + (i+1) + " selecionada, digite o número: "));
+            
+            int numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog(null, (i + 1) + "º Número = Digite um número:", "Inserir 10 números", JOptionPane.QUESTION_MESSAGE));
+
+            numeros[i] = numeroDigitado;
+            soma += numeros[i];
+            contador++;
+        }
+        
+        double media = soma / contador;
+
+        //IO.println("A SOMA de todos os números inseridos é: " + soma);
+        //IO.println("A MÉDIA dos números é: " + media);
+
+        JOptionPane.showMessageDialog(null, "A SOMA de todos os números inseridos é: " + soma + "\nA MÉDIA dos números é: " + media, "Resultado", JOptionPane.WARNING_MESSAGE);
+
+    }
+}
